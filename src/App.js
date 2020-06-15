06ae1c46a2422f6e9b8135621bbd06d4dba230f9
Antonio Lorusso <mitictonylor@gmail.com>
2020-06-15 16:10:45 +0100

    Add Container and create class that is rendered in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import UpcomingFilms from "./containers/UpcomingFilms.js"
 import './App.css';
 
 function App() {
   return (
-    
+<UpcomingFilms></UpcomingFilms>
   );
 }
 
---
+++ src/containers/UpcomingFilms.js
@@ -0,0 +1,12 @@
+import React, {Component, Fragment} from 'react';
+class UpcomingFilms extends Component{
+render(){
+  return(
+
+<p>Upcoming FIlms</p>
+
+
+  )
+}
+}
+export default UpcomingFilms
